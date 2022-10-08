81c460bb411b5811d837029c94ceb7e9b5df4f88
Remi Gau <remi_gau@hotmail.com>
2022-10-08 20:58:30 +0200

    refactor

--- convert_ds_desc_to_datacite.py
+++ convert_ds_desc_to_datacite.py
@@ -1,39 +1,142 @@
 """
+adds a datacite to your BIDS dataset
 details on the format of datacite for GIN: https://gin.g-node.org/G-Node/Info/wiki/DOIfile
 """
 
+from copy import deepcopy
 import json
 from pathlib import Path
 
 import crossref_commons.retrieval
 import requests
 import ruamel.yaml
-from rich import print, Prompt
+from rich import print
+from rich.prompt import Prompt
 
-description = "Example description."
+description = ""
 
-keywords = ["Neuroscience", "Keyword2", "Keyword3"]
-
-output_dir = Path(__file__).parent
-
-ds_descr_file = Path(__file__).parent.joinpath(
-    "test", "data", "dataset_description.json"
-)
+bids_dir = Path(__file__).parent.joinpath("test", "bids")
 
 
 def prompt_format(msg):
-    return f"[bold][underline]{msg}[/bold][/underline]"
+    return f"[bold]{msg}[/bold]"
 
 
 def get_article_info_from_pmid(pmid):
-    print(f"getting info from pubmed for pmid: {pmid}")
     url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id={pmid}&retmode=json"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()["result"][pmid]
 
 
+def update_description(datacite):
+    if description not in [None, ""]:
+        datacite["description"] = description
+    else:
+        datacite["description"] = Prompt.ask(
+            prompt_format("Please enter a description for the dataset")
+        )
+    return datacite
+
+
+def add_license_file(license_type, bids_dir):
+    license_file = bids_dir.joinpath("LICENSE")
+
+    if license_type == "CC0":
+        url = "https://api.github.com/licenses/cc0-1.0"
+
+    license_content = requests.get(url).json()["body"]
+    with license_file.open("w") as f:
+        f.write(license_content)
+
+
+def update_keywords(datacite):
+
+    keywords = deepcopy(datacite["keywords"])
+
+    add_keyword = "yes"
+    while add_keyword == "yes":
+        print(f"Current keywords: {keywords}")
+        add_keyword = Prompt.ask(
+            prompt_format("Do you want to add a keyword?"),
+            default="yes",
+            choices=["yes", "no"],
+        )
+        if add_keyword != "yes":
+            break
+        keyword = Prompt.ask(prompt_format("Please enter a keyword"))
+        keywords.append(keyword)
+
+    for word in keywords:
+        datacite["keywords"].append(word)
+
+    return datacite
+
+
+def update_license(datacite, ds_descr):
+
+    license_file_present = "LICENSE" in bids_dir.glob("LICENSE*")
+
+    if "License" in ds_descr and ds_descr["License"] not in [None, ""]:
+
+        license_name = ds_descr["License"]
+        license_url = ""
+
+        if license_name in ["CC0", "cc0-1.0"]:
+            license_name = (
+                "Creative Commons Zero v1.0 Universal Public Domain Dedication"
+            )
+            license_url = "https://creativecommons.org/publicdomain/zero/1.0/"
+            if not license_file_present:
+                add_license_file("CC0", bids_dir)
+                license_file_present = True
+
+        elif license_name in ["CC-BY-NC-SA-4.0"]:
+            license_name = "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public Domain Dedication"
+            license_url = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+
+        datacite["license"]["name"] = license_name
+        datacite["license"]["url"] = license_url
+
+        if not license_file_present:
+            print(
+                """[red]There is no license file in the dataset directory.
+Please add a license that matches that of the dataset_description.json file.[/red]
+"""
+            )
+
+        return datacite, ds_descr
+
+    if not license_file_present:
+
+        add_license = Prompt.ask(
+            prompt_format("Do you want to add a license?"),
+            default="yes",
+            choices=["yes", "no"],
+        )
+        if add_license == "yes":
+            print(
+                """Possible licences:
+1. CC0
+2. None"""
+            )
+            license = Prompt.ask(
+                prompt_format("Please choose a license."),
+                choices=["1", "2"],
+                default=1,
+            )
+            if license == "1":
+                add_license_file("CC0", bids_dir)
+                ds_descr["License"] = "CC0"
+                (datacite, ds_descr) = update_license(datacite, ds_descr)
+
+        return datacite, ds_descr
+
+
 def main():
+
+    ds_descr_file = bids_dir.joinpath("dataset_description.json")
+    datacite_file = bids_dir.joinpath("datacite.yml")
 
     yaml = ruamel.yaml.YAML()
     yaml.indent(mapping=2, sequence=4, offset=2)
@@ -54,26 +157,13 @@ def main():
         "templateversion": 1.2,
     }
 
+    datacite = update_description(datacite)
+
     if "Authors" in ds_descr:
         for author in ds_descr["Authors"]:
             firstname = author.split(" ")[0]
             lastname = " ".join(author.split(" ")[1:])
             datacite["authors"].append({"firstname": firstname, "lastname": lastname})
-
-    if "License" in ds_descr:
-
-        license_name = ds_descr["License"]
-        license_url = ""
-
-        if license_name in ["CC0"]:
-            license_name = "Creative Commons CC0 1.0 Public Domain Dedication"
-            license_url = "https://creativecommons.org/publicdomain/zero/1.0/"
-        elif license_name in ["CC-BY-NC-SA-4.0"]:
-            license_name = "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public Domain Dedication"
-            license_url = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
-
-        datacite["license"]["name"] = license_name
-        datacite["license"]["url"] = license_url
 
     if "ReferencesAndLinks" in ds_descr:
         for reference in ds_descr["ReferencesAndLinks"]:
@@ -115,8 +205,8 @@ def main():
                         authors.append("et al.")
                         break
 
-            if article_info is not None:
-                print(article_info)
+            # if article_info is not None:
+            #     print(article_info)
 
             if doi is not None or pmid is not None:
                 this_reference[
@@ -129,19 +219,15 @@ def main():
         for grant in ds_descr["Funding"]:
             datacite["funding"].append(grant)
 
-    if description not in [None, ""]:
-        datacite["description"] = description
-    else:
-        Prompt.ask(prompt_format("Please enter a description for the dataset"))
+    (datacite, ds_descr) = update_license(datacite, ds_descr)
 
-    if keywords not in [None, []]:
-        for word in keywords:
-            datacite["keywords"].append(word)
-
-    datacite_file = output_dir.joinpath("datacite.yml")
+    datacite = update_keywords(datacite)
 
     with open(datacite_file, "w") as f:
         yaml.dump(datacite, f)
+
+    with open(ds_descr_file, "w") as f:
+        json.dump(ds_descr, f, indent=4)
 
 
 if __name__ == "__main__":
