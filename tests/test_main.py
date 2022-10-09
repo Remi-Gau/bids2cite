88c683277b28a3618b25f397048a6e6557a967e9
Remi Gau <remi_gau@hotmail.com>
2022-10-09 21:01:33 +0200

    tighten tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,14 +7,25 @@ from bids2cite.bids2cite import update_bidsignore
 def test_update_bidsignore():
 
     bids_dir = get_test_dir().joinpath("bids")
-    update_bidsignore(bids_dir=bids_dir)
 
     bidsignore = bids_dir.joinpath(".bidsignore")
+
+    bidsignore.unlink(missing_ok=True)
+    update_bidsignore(bids_dir=bids_dir)
+    assert bidsignore.exists()
+    with bidsignore.open("r") as f:
+        content = f.read()
+    assert "datacite.yml" in content
+
+    bidsignore.unlink(missing_ok=True)
     with bidsignore.open("w") as f:
         f.write("foo")
     update_bidsignore(bids_dir=bids_dir)
+    with bidsignore.open("r") as f:
+        content = f.read()
+    assert "datacite.yml" in content
 
-    bids_dir.joinpath(".bidsignore").unlink(missing_ok=True)
+    bidsignore.unlink(missing_ok=True)
 
 
 def test_main():
@@ -27,6 +38,9 @@ def test_main():
         skip_prompt=True,
     )
 
-    bids_dir.joinpath(".bidsignore").unlink(missing_ok=True)
+    bidsignore = bids_dir.joinpath(".bidsignore")
+    assert bidsignore.exists()
+    bidsignore.unlink(missing_ok=True)
+
     bids_dir.joinpath("LICENSE").unlink(missing_ok=True)
     bids_dir.joinpath("datacite.yml").unlink(missing_ok=True)
