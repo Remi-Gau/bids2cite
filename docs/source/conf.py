61834b79ae175329f9c404a35340b6037b891551
Remi Gau <remi_gau@hotmail.com>
2024-03-11 10:12:45 -0400

    rm duplicates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.autodoc",
     "sphinx_copybutton",
     "myst_parser",
     "sphinxarg.ext",
